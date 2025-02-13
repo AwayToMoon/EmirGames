4af96fcb45bb4536d9b4989fddb4b7e08080d0a9
AwayToMoon <noblesseraizl@gmail.com>
2025-02-13 15:18:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const games = [
-    { name: "Minecraft", link: "https://minecraft.net", image: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png" },
+    { name: "Life is Strange Remastered", link: "https://store.steampowered.com/app/1265920/Life_is_Strange_Remastered/", image: "https://cdn.discordapp.com/attachments/1267504630884073545/1267672838236667944/bJYCCmymxvgviuuolfOx8srg.png?ex=67aeacd4&is=67ad5b54&hm=065ff82dea913b8dffd6392c1254b68a8e1646f03d90d6c2b7d857a527a62f3f&" },
     { name: "Fortnite", link: "https://www.epicgames.com/fortnite/", image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Fortnite_cover.jpeg/220px-Fortnite_cover.jpeg" },
     { name: "Among Us", link: "https://innersloth.com/gameAmongUs.php", image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Among_Us_cover_art.jpg" }
 ];

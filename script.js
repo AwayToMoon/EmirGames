0dfd7f87e2ab8b5f7fcae7f0919b3e6b02fb8f4d
AwayToMoon <noblesseraizl@gmail.com>
2025-02-13 15:42:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const games = [
-    { name: "Minecraft", link: "https://minecraft.net", image: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png" },
-    { name: "Fortnite", link: "https://www.epicgames.com/fortnite/", image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Fortnite_cover.jpeg/220px-Fortnite_cover.jpeg" },
-    { name: "Among Us", link: "https://innersloth.com/gameAmongUs.php", image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Among_Us_cover_art.jpg" }
-];
-
 const gameList = document.getElementById("game-list");
 const addGameBtn = document.getElementById("add-game-btn");
 const clearGamesBtn = document.getElementById("clear-games-btn");
@@ -13,12 +7,20 @@ const gameNameInput = document.getElementById("game-name");
 const gameLinkInput = document.getElementById("game-link");
 const gameImageInput = document.getElementById("game-image");
 
+let games = JSON.parse(localStorage.getItem("games")) || [
+    { name: "Minecraft", link: "https://minecraft.net", image: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png" },
+    { name: "Fortnite", link: "https://www.epicgames.com/fortnite/", image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Fortnite_cover.jpeg/220px-Fortnite_cover.jpeg" },
+    { name: "Among Us", link: "https://innersloth.com/gameAmongUs.php", image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Among_Us_cover_art.jpg" }
+];
+
 addGameBtn.addEventListener("click", () => {
     gameForm.classList.toggle("hidden");
 });
 
 clearGamesBtn.addEventListener("click", () => {
-    gameList.innerHTML = "";
+    games = [];
+    localStorage.setItem("games", JSON.stringify(games));
+    renderGames();
 });
 
 saveGameBtn.addEventListener("click", () => {
@@ -27,7 +29,9 @@ saveGameBtn.addEventListener("click", () => {
     const image = gameImageInput.value;
 
     if (name && link && image) {
-        addGameToList(name, link, image);
+        games.push({ name, link, image });
+        localStorage.setItem("games", JSON.stringify(games));
+        renderGames();
         gameNameInput.value = "";
         gameLinkInput.value = "";
         gameImageInput.value = "";
@@ -35,18 +39,21 @@ saveGameBtn.addEventListener("click", () => {
     }
 });
 
-function addGameToList(name, link, image) {
-    const div = document.createElement("div");
-    div.classList.add("game");
-    div.innerHTML = `<button class="delete-game">X</button><img src="${image}" alt="${name}" onclick="window.open('${link}', '_blank')"><h2>${name}</h2>`;
-    
-    div.querySelector(".delete-game").addEventListener("click", () => {
-        div.remove();
+function renderGames() {
+    gameList.innerHTML = "";
+    games.forEach((game, index) => {
+        const div = document.createElement("div");
+        div.classList.add("game");
+        div.innerHTML = `<button class="delete-game" data-index="${index}">X</button><img src="${game.image}" alt="${game.name}" onclick="window.open('${game.link}', '_blank')"><h2>${game.name}</h2>`;
+        
+        div.querySelector(".delete-game").addEventListener("click", () => {
+            games.splice(index, 1);
+            localStorage.setItem("games", JSON.stringify(games));
+            renderGames();
+        });
+        
+        gameList.appendChild(div);
     });
-    
-    gameList.appendChild(div);
 }
 
-games.forEach(game => {
-    addGameToList(game.name, game.link, game.image);
-});
+renderGames();
